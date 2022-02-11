a116f6306745dd0d1f78e7b1e6ee80df6235bf8f
Kostiantyn Zhereb <zhereb@gmail.com>
2022-02-11 12:09:59 +0200

    Don't create unused first item in the list
    Empty list is now just nullptr, append handles this cases correctly
    But now we need to update list_head in each call of append_item

--- src/pi13_20220211_linked_list.cpp
+++ src/pi13_20220211_linked_list.cpp
@@ -15,27 +15,33 @@ struct ListNode {
 
 // build list
 ListNode* create_empty_list() {
-	ListNode* head = new ListNode;
-	head->prev = nullptr;
-	head->next = nullptr;
-	head->data = 0; //TODO: don't know what to set here?
-	return head;
+//	ListNode* head = new ListNode;
+//	head->prev = nullptr;
+//	head->next = nullptr;
+//	head->data = 0; //TODO: don't know what to set here?
+//	return head;
+	return nullptr;
 }
 // add item to list
-void append_item(ListNode* list_head, int data) {
+ListNode* append_item(ListNode* list_head, int data) {
 	ListNode* new_node = new ListNode;
 	new_node->data  = data;
 	new_node->next = nullptr;
+	if (list_head == nullptr) {
+		new_node->prev = nullptr;
+		return new_node;
+	}
 	ListNode* current = list_head;
 	while(current->next != nullptr) {
 		current = current->next;
 	}
 	new_node->prev = current;
 	current->next = new_node;
+	return list_head;
 }
 // print list
 void print_list(ListNode* list_head) {
-	ListNode* current = list_head->next; // first node is not used to store data so we need to skip it
+	ListNode* current = list_head;
 	while(current != nullptr) {
 		std::cout<<current->data<<" ";
 		current = current->next;
@@ -50,10 +56,10 @@ void print_list(ListNode* list_head) {
 int main() {
 	ListNode* list_head = create_empty_list();
 
-	append_item(list_head, 1);
-	append_item(list_head, 5);
-	append_item(list_head, 0);
-	append_item(list_head, 7);
+	list_head = append_item(list_head, 1);
+	list_head = append_item(list_head, 5);
+	list_head = append_item(list_head, 0);
+	list_head = append_item(list_head, 7);
 
 	print_list(list_head);
 
