40a1999d26a08ad7f17a04fb6ca1f5d5d68b50be
Kostiantyn Zhereb <zhereb@gmail.com>
2022-02-24 13:31:56 +0200

    Split single file into two files, alternating increasing runs

--- src/pi12_20220224_sort_file.cpp
+++ src/pi12_20220224_sort_file.cpp
@@ -7,8 +7,29 @@
 
 #include <fstream>
 #include <string>
+#include <limits>
 
 namespace pi12_20220224_sort_file {
+
+void split_into_runs(std::string in_path, std::string out_path1, std::string out_path2) {
+	std::ifstream in_file(in_path);
+	std::ofstream out_files[] {std::ofstream(out_path1), std::ofstream(out_path2)};
+	std::size_t current_out = 0;
+	int current;
+	int previous = std::numeric_limits<int>::min();
+	while(true) {
+		in_file>>current;
+		if (in_file.eof()) {break;}
+		if (current > previous) {
+			out_files[current_out] << current<<" ";
+		} else {
+			current_out++;
+			if (current_out > 1) { current_out = 0;}
+			out_files[current_out] << current<<" ";
+		}
+		previous = current;
+	}
+}
 
 void sort_file(std::string file_path) {
 
@@ -30,6 +51,8 @@ void generate_example_file(std::string file_path) {
 int main() {
 	std::string file_path = "example.txt";
 	generate_example_file(file_path);
+
+	split_into_runs("example.txt", "example3.txt", "example4.txt");
 
 	return 0;
 }
