744867ed8ecee6bf55ffec2b9b0d96f639e7e0a4
Kostiantyn Zhereb <zhereb@gmail.com>
2022-04-08 13:28:00 +0300

    Implemented search by type

--- src/pi13_20220408_component_tree.cpp
+++ src/pi13_20220408_component_tree.cpp
@@ -49,6 +49,23 @@ struct ComponentNode {
 		return false;
 	}
 
+	std::vector<ComponentNode*> find_by_type(ComponentType type) {
+		std::vector<ComponentNode*> result;
+		if (this->type == type) {
+			result.push_back(this);
+		}
+		for(std::size_t i = 0; i<subcomponents.size(); i++) {
+			std::vector<ComponentNode*> child_result = subcomponents[i]->find_by_type(type);
+			result.insert(result.end(), child_result.begin(), child_result.end());
+		}
+		return result;
+	}
+
+	std::vector<ComponentNode*> find_by_name_part(std::string name) {
+		std::vector<ComponentNode*> result;
+		return result;
+	}
+
 	void print(int indent_level=0) {
 		for(int i=0; i<indent_level; i++) {std::cout<<"  ";}
 		std::cout<<"{Name="<<name<<",type="<<type<<",cost="<<cost<<",profit="<<profit_per_time;
@@ -82,6 +99,14 @@ int main() {
 	system->print();
 	std::cout<<std::endl;
 
+	std::vector<ComponentNode*> found = system->find_by_type(Basic);
+	std::cout<<"found "<< found.size() << " components"<<std::endl;
+	for(ComponentNode* result : found) {
+		result->print();
+		std::cout<<std::endl;
+	}
+
+	std::cout<<"removing items"<<std::endl;
 	system->remove_subcomponent(basic_producer);
 
 	system->print();
