9b0e17cb427fa89c6aab3da81dbf038e26f10818
Kostiantyn Zhereb <zhereb@gmail.com>
2022-04-14 13:58:37 +0300

    Implemented sum of sparse matrices

--- src/pi12_20220414_sparse_matrix.cpp
+++ src/pi12_20220414_sparse_matrix.cpp
@@ -40,6 +40,10 @@ struct SparseMatrix {
 		assert(row < num_rows);
 		assert(column < num_cols);
 
+		if (data == 0) {
+			return;
+		}
+
 		MatrixNode* current = rows[row];
 		if (!current) {
 			MatrixNode* new_node = new MatrixNode(data, column);
@@ -84,12 +88,37 @@ struct SparseMatrix {
 
 };
 
+SparseMatrix sum(SparseMatrix a, SparseMatrix b) {
+	assert(a.num_cols == b.num_cols);
+	assert(a.num_rows == b.num_rows);
+
+	SparseMatrix result(a.num_rows, a.num_cols);
+	for(std::size_t i = 0; i< a.num_rows; i++) {
+		MatrixNode* current_a = a.rows[i];
+		MatrixNode* current_b = b.rows[i];
+		for (std::size_t j = 0; j<a.num_cols; j++) {
+			int result_data = 0;
+			if (current_a && current_a->column_index == j) {
+				result_data += current_a->data;
+				current_a = current_a->next;
+			}
+			if (current_b && current_b->column_index == j) {
+				result_data += current_b->data;
+				current_b = current_b->next;
+			}
+			result.set(result_data, i, j);
+		}
+	}
+
+	return result;
+}
+
 
 int main() {
 	SparseMatrix matrix(4,4);
 
 	matrix.print();
-	std::cout<<std::endl;
+	std::cout<<"matrix:"<<std::endl;
 
 	matrix.set(1, 1, 0);
 	matrix.set(2, 1, 1);
@@ -102,8 +131,23 @@ int main() {
 
 	matrix.set(8, 3, 1);
 
-
 	matrix.print();
+
+	SparseMatrix matrix2(4,4);
+
+
+	matrix2.set(6, 2, 0);
+	matrix2.set(7, 2, 2);
+
+	matrix2.set(8, 3, 1);
+
+	std::cout<<"matrix2:"<<std::endl;
+	matrix2.print();
+
+	SparseMatrix matrix3 = sum(matrix, matrix2);
+	std::cout<<"sum:"<<std::endl;
+	matrix3.print();
+
 	return 0;
 }
 }
